cdd9ca5244062c91117cc9593a74191790ee5a25
Dylan Corvidae <50845869+dylancorvidae@users.noreply.github.com>
2019-11-18 15:37:55 -0800

    added audio, not playing yet

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,9 @@ const Header = () => (
   <header className={styles.Header}>
     <h1>The X-Files Character Gallery</h1>
     <Link to="/">Home</Link>
+    <div>
+      <audio src='../assets/xsound.mp3' controls autoPlay />
+    </div>
   </header>
 );
 
